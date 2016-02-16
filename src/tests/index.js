314262b901d4080e77df0031aed9adad2f076ce6
Bogdan Chadkin <trysound@yandex.ru>
2016-02-16 14:20:42 +0300

    Use del

--- src/tests/index.js
+++ src/tests/index.js
@@ -8,6 +8,7 @@ import crypto from 'crypto';
 import {_extend} from 'util';
 import Imagemin from 'imagemin';
 import escapeStringRegexp from 'escape-string-regexp';
+import del from 'del';
 
 const src = 'src/tests/src';
 const libSrc = 'src/tests/external_libs';
@@ -48,21 +49,7 @@ function testFileExists(t, file) {
         });
 }
 
-function deleteDest(pathDest = 'src/tests/dest') {
-    if (fs.existsSync(pathDest)) {
-        fs.readdirSync(pathDest).forEach((file) => {
-            const curPath = pathDest + '/' + file;
-            if (fs.lstatSync(curPath).isDirectory()) { // recurse
-                deleteDest(curPath);
-            } else { // delete file
-                fs.unlinkSync(curPath);
-            }
-        });
-        fs.rmdirSync(pathDest);
-    }
-}
-
-deleteDest();
+del.sync('src/tests/dest');
 
 test('options test', (t) => {
     t.plan(3);
