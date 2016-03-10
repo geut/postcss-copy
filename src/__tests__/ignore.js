f6dc961e4a3f80bcf1d160ccc1177e7fd7280ffb
tincho <tinchoz49@gmail.com>
2016-03-10 11:30:37 -0300

    coverage: invalid ignore option

--- src/__tests__/ignore.js
+++ src/__tests__/ignore.js
@@ -3,6 +3,21 @@ import randomFolder from './helpers/random-folder';
 import processStyle from './helpers/process-style';
 import makeRegex from './helpers/make-regex';
 
+test('should keep working if the ignore option is invalid', t => {
+    return processStyle('src/ignore.css', {
+        src: 'src',
+        dest: randomFolder('dest', t.title),
+        template: 'invalid-ignore-option/[path]/[name].[ext]',
+        ignore: 4
+    })
+    .then(result => {
+        const css = result.css;
+        t.regex(css, makeRegex('images/test.jpg?#iefix&v=4.4.0'));
+        t.regex(css, makeRegex('invalid-ignore-option/images/other.jpg'));
+        t.regex(css, makeRegex('invalid-ignore-option/images/noignore.jpg'));
+    });
+});
+
 test('should ignore files with string expression', t => {
     return processStyle('src/ignore.css', {
         src: 'src',
