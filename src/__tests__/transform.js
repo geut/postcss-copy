bb1748bfc23b2f2fbcfb129b71fdad07ec3c5a6b
Martin Acosta <tinchoz49@gmail.com>
2016-11-30 10:23:05 -0300

    added new test for transform function

--- src/__tests__/transform.js
+++ src/__tests__/transform.js
@@ -4,6 +4,35 @@ import Jimp from 'jimp';
 import path from 'path';
 import randomFolder from './helpers/random-folder';
 import processStyle from './helpers/process-style';
+import pify from 'pify';
+
+const stat = pify(fs.stat);
+
+function transform(fileMeta) {
+    const ext = {
+        png: Jimp.MIME_PNG,
+        jpg: Jimp.MIME_JPEG
+    };
+    if (['jpg', 'png'].indexOf(fileMeta.ext) === -1) {
+        return fileMeta;
+    }
+    return Jimp
+        .read(fileMeta.contents)
+        .then(lenna => {
+            return new Promise((resolve, reject) => {
+                lenna
+                .resize(10, 10)
+                .quality(60)
+                .getBuffer(ext[fileMeta.ext], (err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    fileMeta.contents = buffer;
+                    return resolve(fileMeta);
+                });
+            });
+        });
+}
 
 test('should process assets via transform', t => {
     const tempFolder = randomFolder('dest', t.title);
@@ -11,41 +40,36 @@ test('should process assets via transform', t => {
         src: 'src',
         dest: tempFolder,
         template: '[path]/[name].[ext]',
-        transform(fileMeta) {
-            const ext = {
-                png: Jimp.MIME_PNG,
-                jpg: Jimp.MIME_JPEG
-            };
-            if (['jpg', 'png'].indexOf(fileMeta.ext) === -1) {
-                return fileMeta;
-            }
-            return Jimp
-                .read(fileMeta.contents)
-                .then(lenna => {
-                    return new Promise((resolve, reject) => {
-                        lenna
-                        .resize(10, 10)
-                        .quality(60)
-                        .getBuffer(ext[fileMeta.ext], (err, buffer) => {
-                            if (err) {
-                                reject(err);
-                            }
-                            fileMeta.contents = buffer;
-                            return resolve(fileMeta);
-                        });
-                    });
-                });
-        }
+        transform
     })
-    .then(() => {
-        const oldSize = fs
-            .statSync(path.join('src', 'images', 'bigimage.jpg'))
-            .size;
+        .then(() => {
+            const oldStat = stat(
+                path.join('src', 'images', 'bigimage.jpg')
+            );
+            const newStat = stat(
+                path.join(tempFolder, 'images', 'bigimage.jpg')
+            );
 
-        const newSize = fs
-            .statSync(path.join(tempFolder, 'images', 'bigimage.jpg'))
-            .size;
+            return Promise.all([oldStat, newStat]);
+        })
+        .then((results) => {
+            const [oldStat, newStat] = results;
+            return t.truthy(newStat.size < oldStat.size);
+        });
+});
 
-        t.truthy(newSize < oldSize);
-    });
+test(`should process assets via transform and use
+the hash property based on the transform content`, t => {
+    const tempFolder = randomFolder('dest', t.title);
+    return processStyle('src/check-transform.css', {
+        src: 'src',
+        dest: tempFolder,
+        template: '[path]/[hash].[ext]',
+        transform
+    })
+        .then(() => {
+            return stat(
+                path.join(tempFolder, 'images', 'a23a149402afd438.jpg')
+            );
+        });
 });
