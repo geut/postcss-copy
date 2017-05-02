957442b275654c4a3fe1b1319ffcd4b9772b18f2
Martin Acosta <tinchoz49@gmail.com>
2017-05-02 13:19:39 -0300

    refactory tools path and added new tests

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import url from 'url';
 import crypto from 'crypto';
 import micromatch from 'micromatch';
 import copy from './lib/copy';
+import { defineCSSDestPath, findBasePath } from './lib/tools-path';
 
 const tags = ['path', 'name', 'hash', 'ext', 'query', 'qparams', 'qhash'];
 
@@ -26,73 +27,6 @@ function ignore(fileMeta, opts) {
     }
 
     return false;
-}
-
-/**
- * Quick function to find a basePath where the
- * the asset file belongs.
- *
- * @param paths
- * @param pathname
- * @returns {string|boolean}
- */
-function findBasePath(paths, pathname) {
-    for (const item of paths) {
-        if (pathname.indexOf(item) === 0) {
-            return item;
-        }
-    }
-
-    return false;
-}
-
-/**
- * Function to define the dest path of your CSS file
- *
- * @param dirname
- * @param fileMeta
- * @param result
- * @param opts
- * @returns {string}
- */
-function defineCSSDestPath(dirname, fileMeta, result, opts) {
-    const from = path.resolve(result.opts.from);
-    let to;
-
-    if (result.opts.to) {
-        /**
-         * if to === from we can't use it as a valid dest path
-         * e.g: gulp-postcss comes with this problem
-         *
-         */
-        to = path.resolve(result.opts.to);
-        to = to === from ? opts.dest : path.dirname(to);
-    } else {
-        to = opts.dest;
-    }
-
-    if (opts.preservePath) {
-        let srcPath;
-        let basePath;
-
-        if (dirname === path.dirname(from)) {
-            srcPath = dirname;
-            basePath = fileMeta.basePath;
-        } else {
-            /**
-             * dirname !== path.dirname(result.opts.from) means that
-             * postcss-import is grouping different css files in
-             * only one destination, so, the relative path must be defined
-             * based on the CSS file where we read the @imports
-             */
-            srcPath = path.dirname(from);
-            basePath = findBasePath(opts.basePath, from);
-        }
-
-        return path.join(to, path.relative(basePath, srcPath));
-    }
-
-    return to;
 }
 
 /**
@@ -224,7 +158,7 @@ function processUrl(result, decl, node, opts) {
     ).then(() => {
         const destPath = defineCSSDestPath(
             dirname,
-            fileMeta,
+            fileMeta.basePath,
             result,
             opts
         );
